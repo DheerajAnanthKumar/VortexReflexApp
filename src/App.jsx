65e6e6f977741165fd8646c6b5e61ded072749e7
DheerajAnanthKumar <158621436+DheerajAnanthKumar@users.noreply.github.com>
2026-04-13 14:43:19 -0400

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StarsBackground } from "@/components/animate-ui/components/backgrounds/stars";
+
+const App = () => {
+  return (
+    <div>
+      <StarsBackground />
+    </div>
+  );
+};
+
+export default App;
